fef508872ad8c6c105d057eec67178c74091f3b4
ritunegi04 <ritu.negi04@gnail.com>
2020-05-11 18:00:44 +0530

    jenkins chrome browser issue

--- src/test/java/com/qa/testcases/TestClass2.java
+++ src/test/java/com/qa/testcases/TestClass2.java
@@ -4,23 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.qa.base.TestBase;
-
+@Test(groups= {"Sanity"})
 public class TestClass2 extends TestBase {
 	
-	@Test
+	@Test(groups= {"Smoke"})
 	public void check()
 	{
 		log.info("inside check method");
 		System.out.println("check");
 	}
 	
-	@Test
+	
 	public void check1()
 	{
 		log.info("inside check method");
 		System.out.println("check1");
 	}
-	@Test
+	@Test()
 	public void check2()
 	{
 		log.info("inside check method");
